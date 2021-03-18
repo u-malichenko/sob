0ffa2e9574f3cd27a3f9c4143e8786a80dcdef71
u.malichenko <u.malichenko@gmail.com>
2021-03-18 16:51:32 +0300

    dz4

--- src/main/resources/V1__initialize.sql
+++ src/main/resources/V1__initialize.sql
@@ -1,30 +1,39 @@
--- У фильма, который идет в кинотеатре,есть название,длительность (пусть будет 60, 90 или 120 минут),
--- цена билета (в разное время и дни может быть разной),
--- время начала сеанса (один фильм может быть показан несколько раз в разное время и с разной ценой билета).
--- Есть информация о купленных билетах (номер билета, на какой сеанс).
+-- ошибки в расписании (фильмы накладываются друг на друга)
+-- SELECT m.title,
+--        s.time,
+--        l.duration,
+--        m1.title,
+--        s1.time,
+--        l1.duration
 --
--- Составить грамотную нормализованную схему хранения этих данных в БД. Внести в нее 4–5 фильмов,
--- расписание на один день и несколько проданных билетов.
+-- from movies as m
+--          join sessions as s on m.id = s.film_id
+--          join lengths as l on m.length_id = l.id
+--          join movies as m1
+--          join sessions as s1 on m1.id = s1.film_id
+--          join lengths as l1 on m1.length_id = l1.id
 --
--- Сделать запросы, считающие и выводящие в понятном виде:
--- ошибки в расписании (фильмы накладываются друг на друга), отсортированные по возрастанию времени.
--- Выводить надо колонки «фильм 1», «время начала», «длительность», «фильм 2», «время начала», «длительность»;
+-- where s1.time BETWEEN s.time and DATEADD(MINUTE, l.duration, s.time)
+--   and s.time != s1.time
+-- order by s.time
 
--- перерывы 30 минут и более между фильмами — выводить по уменьшению длительности перерыва.
--- Колонки «фильм 1», «время начала», «длительность», «время начала второго фильма», «длительность перерыва»;
+--перерывы 30 минут и более между фильмами —
+-- SELECT m.title      as name,
+--        s.time       as start_time,
+--        l.duration,
+--        s1.time      as next_start_movies,
+--        DATEDIFF(MINUTE, DATEADD(MINUTE, l.duration, s.time), s1.time) as timeout
+--
+-- from SESSIONS as s
+--          join movies as m on m.id = s.film_id
+--          join lengths as l on m.length_id = l.id
+--          join sessions as s1 on s.film_id = s1.film_id - 1
+--          join movies as m1 on m1.id = s1.film_id
+--
+-- where DATEDIFF(MINUTE, DATEADD(MINUTE, l.duration, s.time), s1.time) > 30
+-- order by s.time
 
-SELECT  m.title, s.time, l.duration, DATEADD(MINUTE,  l.duration, s.time) as end,
-        m1.title, s1.time, l1.duration, DATEADD(MINUTE,  l1.duration, s1.time) as end1
 
-from movies as m
-         join sessions as s on m.id = s.film_id
-         join lengths as l on m.length_id= l.id
-
-         join movies as m1
-         join sessions as s1 on m1.id = s1.film_id
-         join lengths as l1 on m1.length_id= l1.id
-
-where s1.time BETWEEN s.time and DATEADD(MINUTE,  l.duration, s.time) and s.time != s1.time;
 create table lengths
 (
     id       serial primary key,
@@ -45,26 +54,26 @@ create table movies
 );
 
 insert into movies (title, length_id)
-values ('Гараж',3),
+values ('Гараж', 3),
        ('Операция Ы', 1),
        ('Терминатор', 2);
 
 create table sessions
 (
     id      bigserial primary key,
-    date    date  not null,
-    time    time  not null,
+    date    date not null,
+    time    time not null,
     film_id bigint references movies (id)
 );
 
 insert into sessions (date, time, film_id)
 values ('2021-03-16', '09:00:00', 1),
-       ('2021-03-16', '09:30:00', 2),
+       ('2021-03-16', '11:30:00', 2),
        ('2021-03-16', '15:00:00', 3);
 
 create table prices
 (
-    id      bigserial primary key,
+    id         bigserial primary key,
     session_id bigint references sessions (id),
     price      float not null
 );
